c1bb4d09ba56d053d759edbfe633dd5f00162f56
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-03-07 15:33:08 -0500

    rutas privadas y publicas

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,5 @@
 import Swal from "sweetalert2";
-import { fetchSinToken } from "../helpers/fetch"
+import { fetchConToken, fetchSinToken } from "../helpers/fetch"
 import { types } from "../types/types";
 
 
@@ -41,7 +41,28 @@ export const startRegister=(name,email,password)=>{
     }
 }
 
+export const startChecking =()=>{
+    return async (dispatch)=>{
+        const resp = await fetchConToken('auth/renew');
+        const body = await resp.json();
+        if (body.ok) {
+            localStorage.setItem('token', body.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+
+            dispatch(login({
+                uid:body.uid,
+                name:body.name
+            }));
+        }else{
+            dispatch(checkingFinish());
+        }
+    }
+}
+
 export const login =(user)=>({
     type:types.authLogin,
     payload:user
+})
+export const checkingFinish =()=>({
+    type:types.authCheckingFinish,
 })
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -16,6 +16,11 @@ export const authReducer =(state= initialState, action)=>{
                 checking:false,
                 ...action.payload
             }
+        case types.authCheckingFinish:
+            return {
+                ...state,
+                checking:false
+            }
 
         default:
             return state;
