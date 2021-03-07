1372d0c2ae1a8e8b9481cdfa2d42d6e584692f20
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-03-06 20:43:40 -0500

    retomando el frontend

--- src/types/types.js
+++ src/types/types.js
@@ -7,6 +7,14 @@ export const types = {
     eventAddNew: '[event] Add New',
     eventClearActiveEvent:'[event] Clear Active Event',
     eventUpdated:'[event] Event Updated',
-    eventDeleted:'[event] Event Deleted'
+    eventDeleted:'[event] Event Deleted',
+
+    authChecking:'[auth] Checking login state',
+    authCheckingFinish:'[auth] Finish Checking login state',
+    authStartLogin:'[auth] Start login',
+    authLogin:'[auth] Login',
+    authStartRegister:'[auth] Start register',
+    authStartTokenRenew:'[auth] Start Token Renew',
+    authLogout:'[auth] Logout'
 
 }
